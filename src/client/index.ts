19f98a53be25a7e5db9df4bf243be4aa596aaa77
xWenyl <wenylpolska@gmail.com>
2023-01-29 18:24:34 +0100

    Create client

---
+++ src/client/index.ts
@@ -0,0 +1,20 @@
+import { Client, GatewayIntentBits } from 'discord.js'
+import { registerEvents } from '../utils'
+import keys from '../keys'
+import events from '../events'
+
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+})
+
+registerEvents(client, events)
+
+client.login(keys.clientToken)
+    .catch((err) => {
+        console.error('[Login Error]', err)
+        process.exit(1)
+    })
