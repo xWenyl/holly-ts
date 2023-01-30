91ec2e03ca4b37abfd1b53f2e63bb0c5705d7161
xWenyl <wenylpolska@gmail.com>
2023-01-30 19:14:35 +0100

    Update keys

--- src/keys/index.ts
+++ src/keys/index.ts
@@ -1,7 +1,9 @@
 import { Keys } from '../types'
 const keys: Keys = {
-    clientToken: process.env.CLIENT_TOKEN ?? "nil"
+    clientToken: process.env.CLIENT_TOKEN ?? "nil",
+    testGuild: process.env.TEST_GUILD ?? "nil"
 }
+
 
 if (Object.values(keys).includes('nil'))
     throw new Error('Not all ENV variables are defined!')
